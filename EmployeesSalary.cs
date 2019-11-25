9e0d9e6efabf82407a7d6804c2cd3839ee30658d
naswirushema <naswirushema@gmail.com>
2019-11-25 18:02:55 +0200

    First version

---
+++ EmployeesSalary.cs
@@ -0,0 +1,63 @@
+﻿namespace salary
+{
+    public class EmployeesSalary
+    {
+        private int id;
+        private string name;
+        private string position;
+        private string gender;
+
+        public EmployeesSalary(int id, string name, string position, string gender)
+        {
+            this.id = id;
+            this.name = name;
+            this.position = position;
+            this.gender = gender;
+        }
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string Position
+        {
+            get => position;
+            set => position = value;
+        }
+
+        public string Gender
+        {
+            get => gender;
+            set => gender = value;
+        }
+
+        public double MonthlySalary(double grossSalary)
+        {
+            double salary;
+            salary = grossSalary - (((grossSalary * 30) / 100) + ((grossSalary * 5) / 100) + ((grossSalary * 3) / 100));
+            return salary;
+        }
+
+        public double HourlySalary(double rate, double hour)
+        {
+            double salary;
+            salary=(rate*hour)-((((rate*hour)*5)/100)+(((rate*hour)*3)/100));
+            return salary;
+        }
+
+        public double OneTimeSalary(double grossSalary)
+        {
+            double salary;
+            salary = grossSalary - ((grossSalary * 30) / 100);
+            return salary;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int id;
+            string name;
+            string position;
+            string gender;
+
+            //   EmployeesSalary cal = new EmployeesSalary();
+
+            Console.WriteLine("please enter your id number");
+            id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("your name please");
+            name = Console.ReadLine();
+            Console.WriteLine("what is your position");
+            position = Console.ReadLine();
+            Console.WriteLine("what is your gender");
+            gender = Console.ReadLine();
+            EmployeesSalary cal = new EmployeesSalary(id, name, position, gender);
+            Console.WriteLine("enter the gross salary");
+            double grossSalary;
+            grossSalary = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("what type of employee are you? 1=monthly, 2=one time, 3= hourly");
+            int type;
+            type = Convert.ToInt32(Console.ReadLine());
+            if (type == 1)
+            {
+                Console.WriteLine(
+                    $"{cal.Name} your monthly salary on position of {cal.Position}   is {cal.MonthlySalary(grossSalary)}");
+
+            }
+            else if (type == 2)
+            {
+
+                Console.WriteLine(
+                    $"{cal.Name} your one-time salary on position of {cal.Position} is {cal.OneTimeSalary(grossSalary)}");
+            }
+            else if (type == 3)
+            {
+
+
+                Console.WriteLine("how many hours have you worked? ");
+                double hours;
+
+
+                hours = Convert.ToDouble(Console.ReadLine());
+                if (hours > 0)
+                {
+                    Console.WriteLine($"{cal.Name} Your hourly salary is {cal.HourlySalary(grossSalary, hours)}");
+                }
+                else
+                {
+                    Console.WriteLine(" Your hourly salary is 0");
+                }
+
+
+
+
+
+            }
+        }
+    }
+}
